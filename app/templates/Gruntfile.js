6b96397c656f2908e67f9d1ba061c136f0f10a2a
louxiaojian <louxiaojian@gmail.com>
2014-10-15 10:29:41 +0800

    update

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -11,6 +11,7 @@ module.exports = function(grunt) {
     livereload: 35729,
     static: {
       request: null,
+      location: null,
       reqUrl: '',
       debug: false,
       staticAsset: '<%= staticAsset %>',
